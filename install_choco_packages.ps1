b2c71cd6ec2898edfab4bb3e156f3794c834bcc6
Yunolan <thorpejoe4@gmail.com>
2018-07-21 13:11:54 +0800

    Chocoinstall packages now working

--- install_choco_packages.ps1
+++ install_choco_packages.ps1
@@ -1,7 +1,8 @@
 choco feature enable -n allowGlobalConfirmation
 Import-Csv "packages.csv" | Foreach-Object {
-    foreach ($package in $_.PSObject.Properties)
+    foreach ($line in $_.PSObject.Properties)
     {
+        $package = $line.Value
         choco install $package
     }
 }
